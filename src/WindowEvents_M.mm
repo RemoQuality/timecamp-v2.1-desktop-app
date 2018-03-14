1d388153eded82d950187195c7addb3e904e51b0
karololszak <k.olszacki@timecamp.com>
2018-03-14 13:16:37 +0100

    macOS getIdleTime port from old app (and secs -> millisecs)

--- src/WindowEvents_M.mm
+++ src/WindowEvents_M.mm
@@ -15,7 +15,33 @@
 
 unsigned long WindowEvents_M::getIdleTime()
 {
-    return 0;
+    int64_t idlesecs = -1;
+    io_iterator_t iter = 0;
+
+    if (IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching("IOHIDSystem"), &iter) == KERN_SUCCESS)
+    {
+        io_registry_entry_t entry = IOIteratorNext(iter);
+        if (entry)
+        {
+            CFMutableDictionaryRef dict = NULL;
+            if (IORegistryEntryCreateCFProperties(entry, &dict, kCFAllocatorDefault, 0) == KERN_SUCCESS)
+            {
+                CFNumberRef obj = (CFNumberRef) CFDictionaryGetValue(dict, CFSTR("HIDIdleTime"));
+                if (obj)
+                {
+                    int64_t nanoseconds = 0;
+                    if (CFNumberGetValue(obj, kCFNumberSInt64Type, &nanoseconds))
+                    {
+                        idlesecs = (nanoseconds / 1000000); // Convert from nanoseconds to milliseconds
+                    }
+                }
+                CFRelease(dict);
+            }
+            IOObjectRelease(entry);
+        }
+        IOObjectRelease(iter);
+    }
+    return (unsigned long) idlesecs;
 }
 
 void WindowEvents_M::run()
