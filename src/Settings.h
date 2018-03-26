144dac8252b331740fbeb5df5034137e4d20dafc
Karol @ Mac <k.olszacki@timecamp.com>
2018-03-26 16:34:32 +0200

    use ico file (icns is mac-only)

--- src/Settings.h
+++ src/Settings.h
@@ -15,7 +15,7 @@
 #define NO_TIMER_TEXT "0:00"
 
 #define MAIN_ICON ":/Icons/res/AppIcon.ico"
-#define MAIN_BG ":/Icons/res/AppIcon.icns"
+#define MAIN_BG ":/Icons/res/AppIcon.ico"
 
 // db params
 #define DB_FILENAME "localdb.sqlite"
