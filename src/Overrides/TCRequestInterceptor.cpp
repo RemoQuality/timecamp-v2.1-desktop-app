3a40d4e3a4d1e397e615e3591fd0f14e8b62a3e0
karololszak <k.olszacki@timecamp.com>
2018-04-12 15:11:29 +0200

    Redirect logout -> to login page

--- src/Overrides/TCRequestInterceptor.cpp
+++ src/Overrides/TCRequestInterceptor.cpp
@@ -1,6 +1,8 @@
 #include "TCRequestInterceptor.h"
 #include "src/Settings.h"
 
+#include <QDebug>
+
 TCRequestInterceptor::TCRequestInterceptor(QObject *p) : QWebEngineUrlRequestInterceptor(p)
 {
 
@@ -9,4 +11,11 @@ TCRequestInterceptor::TCRequestInterceptor(QObject *p) : QWebEngineUrlRequestInt
 void TCRequestInterceptor::interceptRequest(QWebEngineUrlRequestInfo &info)
 {
     info.setHttpHeader(CONN_CUSTOM_HEADER_NAME, CONN_CUSTOM_HEADER_VALUE);
+//    qDebug() << "[TCRequestInterceptor] URL: " << info.requestUrl();
+    // if exactly the login page (so after logout)
+    if(QString::compare(info.requestUrl().toString(), "https://www.timecamp.com/", Qt::CaseInsensitive) == 0){
+        qDebug() << "[TCRequestInterceptor] URL: " << info.requestUrl().toString();
+        qDebug() << "[TCRequestInterceptor] Redirect logout -> to login page";
+        info.redirect(QUrl("https://www.timecamp.com/auth/login"));
+    }
 }
