2a52890621440523b1984b21c5864245454195b7
Karol Olszacki <karololszak+github@gmail.com>
2018-04-13 09:33:37 +0200

    deploy win with debinfo version (to have dbg symbols)

--- deploy_win.bat
+++ deploy_win.bat
@@ -4,7 +4,7 @@ del /s /q deploy
 rmdir /s /q deploy
 mkdir deploy
 mkdir deploy\resources
-copy "cmake-build-release-visual-studio\TimecampDesktop.exe" "deploy"
+copy "cmake-build-relwithdebinfo-visual-studio\TimecampDesktop.exe" "deploy"
 copy "src\resources\programicon.ico" "deploy\resources\programicon.ico"
 REM copy "src\localdb.sqlite" "deploy\localdb.sqlite"
 C:\Qt\5.10.0\msvc2017_64\bin\windeployqt.exe deploy
