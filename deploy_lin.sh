194327fa90514d744199caec40fa976d35618db6
karololszak <k.olszacki@timecamp.com>
2018-04-16 10:45:50 +0200

    final, working version for deb/ubu/fedora

--- deploy_lin.sh
+++ deploy_lin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # add Qt bin to your $PATH
 
-SRC="cmake-build-relwithdebinfo-gcc"
+SRC="cmake-build-relwithdebinfo-gcc-48"
 TARGET="linuxdeployqt/TimecampDesktop.AppDir"
 CMAKE_NAME="TimecampDesktop"
 BUNDLE_NAME="TimecampDesktop"
@@ -25,9 +25,21 @@ cp $SRC/"${CMAKE_NAME}" $TARGET"/usr/bin/""${BUNDLE_NAME}" # copy the binary
 cp res"/${BUNDLE_NAME}.desktop" $TARGET"/usr/share/applications/${BUNDLE_NAME}.desktop" # copy app.desktop file
 cp "res/AppIcon128.png" $TARGET"/usr/share/icons/hicolor/48x48/${BUNDLE_NAME}.png" # copy app.desktop file
 
+#source /opt/qt*/bin/qt*-env.sh ## call it from shell yourself
+
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+
 chmod a+x "${LinuxDeployQtExec}"
-${LinuxDeployQtExec} $TARGET"/usr/share/applications/${BUNDLE_NAME}.desktop" -bundle-non-qt-libs -verbose=2
 cp /lib/x86_64-linux-gnu/libssl.so.1.0.0  $TARGET"/usr/lib/"
 cp /lib/x86_64-linux-gnu/libcrypto.so.1.0.0  $TARGET"/usr/lib/"
+cp /lib/x86_64-linux-gnu/libbsd.so.0 $TARGET"/usr/lib/"
+#cp /lib/x86_64-linux-gnu/libexpat.so.1 $TARGET"/usr/lib/"
+#cp /lib/x86_64-linux-gnu/libz.so.1 $TARGET"/usr/lib/"
+#cp /lib/x86_64-linux-gnu/libglib-2.0.so.0 $TARGET"/usr/lib/"
+#cp /lib/x86_64-linux-gnu/libpcre.so.3 $TARGET"/usr/lib/"
+${LinuxDeployQtExec} $TARGET"/usr/share/applications/${BUNDLE_NAME}.desktop" -bundle-non-qt-libs -extra-plugins=sqldrivers,iconengines -verbose=2
 ${LinuxDeployQtExec} $TARGET"/usr/share/applications/${BUNDLE_NAME}.desktop" -appimage -verbose=2
 #${AppImageToolExec} $TARGET
+
+echo "Unbundled dependant libs (they need to be in the system!):"
+find $TARGET -executable -type f -exec ldd {} \; | grep -v "/home" | sort | uniq
