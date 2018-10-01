20dce39850e2b89df827c26967b225f24ce2b096
karololszak <k.olszacki@timecamp.com>
2018-10-01 16:47:39 +0200

    fix

--- install_deb.sh
+++ install_deb.sh
@@ -5,7 +5,7 @@ sudo apt-get install libc-bin
 
 sudo pkill -f TimeCampDesktop
 
-sudo dpkg -i *.deb
+sudo dpkg -i ./*.deb
 
 #xdg-icon-resource install --novendor --size 256 /usr/share/timecamp/icons/tc_tracking.png tc_tracking
 #xdg-icon-resource install --novendor --size 256 /usr/share/timecamp/icons/tc_tracking_task.png tc_tracking_task
